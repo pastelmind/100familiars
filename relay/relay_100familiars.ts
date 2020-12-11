dc274995be32a0aa17e46e0128acd75afef43bfd
pastelmind <keepyourhonor@gmail.com>
2020-12-11 19:41:22 +0900

    feat: Port relay script to TypeScript

---
+++ relay/relay_100familiars.ts
@@ -0,0 +1,235 @@
+/**
+ * @file Display your familiars and your best ascension run records with them.
+ *
+ * This script is largely based on matt.chugg's Familiar Collector/Ascension
+ * Familiar Chooser script. For his work, see:
+ * https://kolmafia.us/threads/familiar-collector-ascension-familiar-chooser.7433/
+ */
+
+import {
+  getRevision,
+  haveFamiliar,
+  myId,
+  print,
+  toFamiliar,
+  toString as formatString,
+  visitUrl,
+  write,
+  xpath,
+} from "kolmafia";
+
+/**
+ * Represents an exception thrown when the current KoLmafia version does not
+ * match an expected condition.
+ */
+class KolmafiaVersionError extends Error {
+  constructor(message?: string) {
+    super(message);
+  }
+}
+
+sinceKolmafiaRevision(20550);
+
+/**
+ * Represents information about a single familiar % run.
+ */
+interface FamiliarRunInfo {
+  /**
+   * Highest familiar % ever achieved in a run with this familiar
+   */
+  bestRunPercent: number;
+}
+
+/**
+ * Retrieves your highest familiar % run records.
+ * For convenience, this does NOT check pre-NS13 ascension records.
+ * @returns Mapping of familiar => best familiar run info
+ */
+function getFamiliarRuns(): Map<Familiar, FamiliarRunInfo> {
+  const runs = new Map<Familiar, FamiliarRunInfo>();
+
+  const page = visitUrl("ascensionhistory.php?who=" + myId());
+  const nodes = xpath(page, '//table[@id="history"]//tr[position() > 1]//img');
+
+  for (let node of nodes) {
+    let match = /title="(.+?)\s*\((.+?)%/.exec(node);
+    // Some challenge paths (e.g. Avatar of Boris) can have no familiar records
+    if (!match) continue;
+
+    // Use toFamiliar() because Familiar.get() crashes if it encounters an
+    // unknown familiar name
+    let familiarName = match[1];
+    let fam = toFamiliar(familiarName);
+    let runPercent = parseFloat(match[2]);
+
+    // Skip unknown familiar
+    if (fam === Familiar.get("none")) {
+      print("Unknown familiar name: " + familiarName, "red");
+      continue;
+    }
+
+    // Skip if this is not the best run for this familiar
+    let prevRecord = runs.get(fam);
+    if (prevRecord && prevRecord.bestRunPercent >= runPercent) {
+      continue;
+    }
+
+    runs.set(fam, { bestRunPercent: runPercent });
+  }
+
+  return runs;
+}
+
+/**
+ * Check your terrarium to see what familiars you have.
+ * This function is needed because `haveFamiliar()` always returns `false` if
+ * you are in a challenge path that restricts familiars (e.g. Avatar of Boris).
+ * @returns Array of familiars in your terrarium. This may not include your
+ *    currently active familiar.
+ */
+function getTerrarium(): Familiar[] {
+  const terrariumFamiliars: Familiar[] = [];
+  const page = visitUrl("familiar.php");
+  const familiarIdPattern = /fam\((\d+)\)/g;
+  let match;
+
+  while ((match = familiarIdPattern.exec(page))) {
+    // As of r20550, toFamiliar() in JS has a bug where many familiar IDs
+    // incorrectly return Familiar.get("none").
+    // This is why we're using Familiar.get() here, despite knowing that it will
+    // crash if KoLmafia encounters an unknown familiar ID.
+    let familiarId = Number(match[1]);
+    // @ts-expect-error kolmafia-js 1.0.3 is missing type definitions for
+    // Familiar.get() that accepts numbers
+    let fam = Familiar.get(familiarId);
+
+    // Skip unknown familiar
+    if (fam === Familiar.get("none")) {
+      print("Unknown familiar ID: " + familiarId, "red");
+      continue;
+    }
+
+    terrariumFamiliars.push(fam);
+  }
+
+  return terrariumFamiliars;
+}
+
+/**
+ * Generates a sortable HTML table of all familiars.
+ * @returns {string} HTML for the familiar table
+ */
+function generateFamiliarTable() {
+  let html = `
+<table class="familiars">
+  <thead>
+    <tr>
+      <th class="no-sort" data-sort-method="none"></th>
+      <th>Familiar</th>
+      <th>Owned?</th>
+      <th>Best Run %</th>
+    </tr>
+  </thead>
+  <tbody>`;
+
+  const familiarRuns = getFamiliarRuns();
+  const terrariumFamiliars = new Set(getTerrarium());
+
+  for (let fam of Familiar.all()) {
+    let bestRunText = "";
+    let runPercentClasses = "col-run-pct";
+    let ownedSymbol;
+    let ownedClasses = "col-owned";
+
+    if (
+      haveFamiliar(fam) ||
+      terrariumFamiliars.has(fam) ||
+      familiarRuns.has(fam)
+    ) {
+      let bestRunRecord = familiarRuns.get(fam);
+      if (bestRunRecord) {
+        let { bestRunPercent } = bestRunRecord;
+        bestRunText = formatString(bestRunPercent, "%.1f");
+
+        if (bestRunPercent === 100) {
+          // Perfect run
+          runPercentClasses += " col-run-pct--perfect";
+        } else if (bestRunPercent >= 90 && bestRunPercent < 100) {
+          // Contributes to an Amateur/Professional Tour Guide trophy
+          runPercentClasses += " col-run-pct--tourguide";
+        }
+      }
+
+      ownedSymbol = "&#x2714;"; // Checkmark
+      ownedClasses += " col-owned--yes";
+    } else {
+      ownedSymbol = "&#x2718;"; // X mark
+      ownedClasses += " col-owned--no";
+    }
+
+    html += `
+    <tr>
+      <td class="col-img"><img src="/images/itemimages/${fam.image}"></td>
+      <td>${String(fam)}</td>
+      <td class="${ownedClasses}">${ownedSymbol}</td>
+      <td class="${runPercentClasses}">${bestRunText}</td>
+    </tr>`;
+  }
+
+  html += `
+  </tbody>
+</table>
+`;
+  return html;
+}
+
+/**
+ * Entrypoint of the relay script
+ */
+export function main() {
+  write(`<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>100familiars</title>
+    <script src="/100familiars/tablesort.min.js"></script>
+    <script src="/100familiars/tablesort.number.min.js"></script>
+    <link rel="stylesheet" href="/100familiars/tablesort.css">
+    <link rel="stylesheet" href="/100familiars/style.css">
+  </head>
+  <body>`);
+
+  write(generateFamiliarTable());
+
+  write(`
+  <script>
+    new Tablesort(document.getElementsByClassName('familiars')[0]);
+  </script>
+  </body>
+</html>`);
+}
+
+/**
+ * Checks if the current KoLmafia's revision number is same or greater than
+ * `revision`.
+ * This behaves like the `since rXXX;` statement in ASH.
+ * @param revision Revision number
+ * @throws {KolmafiaVersionError} If the current KoLmafia's revision number is
+ *    less than `revision`.
+ * @throws {TypeError} If `revision` is not an integer
+ */
+function sinceKolmafiaRevision(revision: number) {
+  if (!Number.isInteger(revision)) {
+    throw new TypeError(
+      `Invalid revision number ${revision} (must be an integer)`
+    );
+  }
+
+  // Based on net.sourceforge.kolmafia.textui.Parser.sinceException()
+  if (getRevision() < revision) {
+    throw new KolmafiaVersionError(
+      `This script requires revision r${revision} of kolmafia or higher (current: ${getRevision()}). Up-to-date builds can be found at https://ci.kolmafia.us/.`
+    );
+  }
+}
