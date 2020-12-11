26e32bbf790ac951366b60904d74550ecbae7e74
pastelmind <keepyourhonor@gmail.com>
2020-12-12 04:49:28 +0900

    build: Update Rollup config to bundle vhtml

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,15 @@
+import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 
 /** @type {import("rollup").RollupOptions} */
 const options = {
-  input: "relay/relay_100familiars.ts",
+  input: "relay/relay_100familiars.tsx",
   output: {
     dir: "relay",
     format: "cjs",
   },
   external: "kolmafia",
-  plugins: [typescript()],
+  plugins: [commonjs(), typescript()],
 };
 
 export default options;
