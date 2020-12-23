38262e04566c09453515bb36b85c6daa5cba5293
pastelmind <keepyourhonor@gmail.com>
2020-12-24 03:06:03 +0900

    feat: Use kolmafia-util's sinceKolmafiaVersion()

--- relay/relay_100familiars.tsx
+++ relay/relay_100familiars.tsx
@@ -8,7 +8,6 @@
 
 import {
   entityDecode,
-  getRevision,
   haveFamiliar,
   myId,
   print,
@@ -18,18 +17,9 @@ import {
   write,
   xpath,
 } from "kolmafia";
+import { sinceKolmafiaRevision } from "kolmafia-util";
 
 import h from "vhtml";
-
-/**
- * Represents an exception thrown when the current KoLmafia version does not
- * match an expected condition.
- */
-class KolmafiaVersionError extends Error {
-  constructor(message?: string) {
-    super(message);
-  }
-}
 
 sinceKolmafiaRevision(20550);
 
@@ -257,28 +247,4 @@ export function main(): void {
       </html>
     )
   );
-}
-
-/**
- * Checks if the current KoLmafia's revision number is same or greater than
- * `revision`.
- * This behaves like the `since rXXX;` statement in ASH.
- * @param revision Revision number
- * @throws {KolmafiaVersionError} If the current KoLmafia's revision number is
- *    less than `revision`.
- * @throws {TypeError} If `revision` is not an integer
- */
-function sinceKolmafiaRevision(revision: number): void {
-  if (!Number.isInteger(revision)) {
-    throw new TypeError(
-      `Invalid revision number ${revision} (must be an integer)`
-    );
-  }
-
-  // Based on net.sourceforge.kolmafia.textui.Parser.sinceException()
-  if (getRevision() < revision) {
-    throw new KolmafiaVersionError(
-      `This script requires revision r${revision} of kolmafia or higher (current: ${getRevision()}). Up-to-date builds can be found at https://ci.kolmafia.us/.`
-    );
-  }
 }
